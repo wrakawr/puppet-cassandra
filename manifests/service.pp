63ff9f8fe22a71943e8c66b79ce390cd6156da5f
Evgeny Syrtsov <es@infopunks.com>
2013-08-10 13:31:58 +0200

    custom status provided, cause datastax status always provides 0 exit code

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,6 +3,7 @@ class cassandra::service {
         ensure     => running,
         enable     => true,
         hasstatus  => true,
+        status => '/bin/bash -c ". /etc/rc.d/init.d/functions && status -p /var/run/cassandra/cassandra.pid cassandra"',
         hasrestart => true,
         subscribe  => Class['cassandra::config'],
         require    => Class['cassandra::config'],
